4573216d2445d2d7b0198c672130631292123d1a
91994 <91994@DESKTOP-ENP0TJU>
2022-08-28 12:55:48 +0530

    First commit

---
+++ Operators/src/com/cg/operators/Task1.java
@@ -0,0 +1,15 @@
+package com.cg.operators;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		int a=5,b=18;
+		if((a=18)==b)
+		{ System.out.println(++a);}
+		else
+		{ System.out.println(++b); }
+
+
+	}
+
+}
---
+++ Operators/src/com/cg/operators/Task3.java
@@ -0,0 +1,13 @@
+package com.cg.operators;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+        float f = (1 / 4) * 10;
+        int i =(int) f;
+        System.out.println(i);
+
+
+	}
+
+}
